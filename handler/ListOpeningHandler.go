4d553ee2f1585afa7e4fb9fcaf91c294dde883b0
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-16 19:15:19 -0300

    feat: add separate  files for route handlers

---
+++ handler/ListOpeningHandler.go
@@ -0,0 +1,12 @@
+package handler
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func ListOpeningHandler(ctx *gin.Context){
+	ctx.JSON(http.StatusOK,gin.H{
+		"msg":"GET Openings",
+	})
+}
