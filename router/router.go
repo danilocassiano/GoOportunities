9776ee87a32edea1875b02b1dfd2374f7683907c
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-16 18:45:10 -0300

    feat: creating the initial structure of the Routes

--- router/router.go
+++ router/router.go
@@ -3,12 +3,13 @@ package router
 import "github.com/gin-gonic/gin" 
 
 func Inicialize(){
+
+	//Initializer router.
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	initializeRoutes(router)
 	
+	
+	//Run the server.
 	router.Run(":8080")
 }
