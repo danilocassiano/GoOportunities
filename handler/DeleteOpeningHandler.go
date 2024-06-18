98b091ee6f1f86e4b4717668b89e239773228a55
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-17 22:35:00 -0300

    feat: feat: create endpoint for Create routine

--- handler/DeleteOpeningHandler.go
+++ handler/DeleteOpeningHandler.go
@@ -1,13 +1,31 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/danilocassiano/GoOportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func DeleteOpeningHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK,
-	gin.H{
-		"msg":"DELETE Opening",
-	})
+func DeleteOpeningHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+	
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting opening with id: %s", id))
+		return
+	}
+
+	sendSuccess(ctx, "deleting-opening", opening)
 }
