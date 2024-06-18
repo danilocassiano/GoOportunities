7374f5bb289a02f0444d3597fa7eb757a4e8d56c
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-17 22:37:48 -0300

    feat: add general handler with validation for Create and Update routines

--- handler/handler.go
+++ handler/handler.go
@@ -47,3 +47,22 @@ func (r*CreateOpeningRequest) Validate()error {
 
 	return nil
 }
+
+//Updated
+
+type UdpdateOpeningRequest struct {
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    *bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+}
+
+func (r *UdpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Link != "" || r.Remote != nil || r.Salary > 0 {
+		return nil
+	}
+
+	return fmt.Errorf("at least one valid field must be provided")
+}
