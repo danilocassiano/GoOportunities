b4452b13cb9bf069d6108fe621b9f4743ccb7c7d
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-17 22:35:42 -0300

    feat: create endpoint for List routine

--- handler/ListOpeningHandler.go
+++ handler/ListOpeningHandler.go
@@ -2,11 +2,18 @@ package handler
 
 import (
 	"net/http"
+
+	"github.com/danilocassiano/GoOportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK,gin.H{
-		"msg":"GET Openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error liting openings")
+		return
+	}
+
+	sendSuccess(ctx, "Llinting-openings", openings)
 }
