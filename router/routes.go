08f0270ce6d8db738ccec1fe5ff5a38895a8b256
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-18 14:13:45 -0300

    feat: create documetation swagger

--- router/routes.go
+++ router/routes.go
@@ -3,14 +3,19 @@ package router
 import (
 	"github.com/danilocassiano/GoOportunities/handler"
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+	"github.com/danilocassiano/GoOportunities/docs"
 ) 
 
 func initializeRoutes(router *gin.Engine){
 	
 	//initialize handler
 	handler.InitializeHandler()
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 
-	v1 := router.Group("/api/v1")
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -18,5 +23,10 @@ func initializeRoutes(router *gin.Engine){
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningHandler)
 	}	
+
+	//inicialize Swagger
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	
 }
 
