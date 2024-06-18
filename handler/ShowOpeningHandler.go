21b12cafaf6a3aea9cf85fdab9e6aaa98e0449fc
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-06-17 22:35:22 -0300

    feat: create endpoint for Show routine

--- handler/ShowOpeningHandler.go
+++ handler/ShowOpeningHandler.go
@@ -2,11 +2,24 @@ package handler
 
 import (
 	"net/http"
+
+	"github.com/danilocassiano/GoOportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error;err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-success", opening)
 }
